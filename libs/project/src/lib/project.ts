f7894922c5f8051b7644ada46c426e81ce49e3eb
Devashish Lal <laldevashish@gmail.com>
2019-08-28 13:15:26 +0530

    project unit test created

--- libs/project/src/lib/project.ts
+++ libs/project/src/lib/project.ts
@@ -1,17 +1,46 @@
+import { readFile, writeFile } from '@ocular/promise';
+
+export interface ProjectData {
+  name: string,
+  path: string
+}
+
 export class Project {
 
   private name: string;
+  private path: string;
 
-  constructor(name: string) {
-    this.name = name;
+  constructor(data: ProjectData) {
+    this.name = data.name;
+    this.path = data.path + data.name + '.osproj';
+  }
+
+  public static async load(path: string): Promise<Project> {
+    try {
+      const data: ProjectData = await readFile(path);
+      return new Project(data);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
+
+  public async save(): Promise<boolean> {
+    const data = {
+      name: this.name
+    };
+
+    try {
+      await writeFile(this.path, data);
+      return true;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
   }
 
   public get Name(): string {
     return this.name;
-  }
-
-  public save(): boolean {
-    return false;
   }
 
 }
--- libs/promise/src/index.ts
+++ libs/promise/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/promise';
+export * from './lib/jsonfile';
